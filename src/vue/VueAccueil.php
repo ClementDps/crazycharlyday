50bec33efa786cfbdf3353f0fe0c3749ec3a9bdd
Lucas <lucas.briand1@etu.univ-lorraine.fr>
2018-02-08 19:19:32 +0100

    css

--- src/vue/VueAccueil.php
+++ src/vue/VueAccueil.php
@@ -31,23 +31,24 @@ public function render(){
       $html = \garagesolidaire\vue\VueGeneral::genererHeader("demarrage");
       $html .=
       <<<END
-  <h1>Bienvenue sur le site du <strong>garage solidaire</strong></h1>
-  <h3> garagesolidaire est un site qui permet de réserver un garage afin de réparer sa voiture </h3>
-  <p> Groupe : Briand Lucas | Claisse Julien | Combe-Deschaumes Sarah | Domps Clément | Millotte Baptiste </p>
+  <h1>Bienvenue sur le site du <strong>Garage Solidaire</strong> !</h1>
+  <h3> Le Garage Solidaire est un site qui permet de réserver un garage afin de réparer sa voiture seul ou avec un de
+  nos nombreux professionnels. Chaque client peut aussi louer un de nos véhicules.</h3>
+  <p> Développé par : Briand Lucas | Claisse Julien | Combe-Deschaumes Sarah | Domps Clément | Millotte Baptiste </p>
 END;
     break;
 
     case VueAccueil::AFF_ABOUT:
     $html = \garagesolidaire\vue\VueGeneral::genererHeader("about");
     $html .= <<<END
-    <h1>A propos de nous</h1>
+    <h1>À propos de nous</h1>
     <h3>Qui sommes-nous ?</h3>
     <p>Nous sommes 5 étudiants en deuxième année de DUT Informatique à Nancy.<BR>
-      Groupe B : Briand Lucas | Claisse Julien | Combe-Deschaumes Sarah | Domps Clément | Millotte Baptiste </p>
+      Groupe : Briand Lucas | Claisse Julien | Combe-Deschaumes Sarah | Domps Clément | Millotte Baptiste </p>
     <h3>Pourquoi ?</h3>
     <p>Ce site internet est un des projets que nous avons à faire au cours de
       notre année d'étude. <BR>Il nous a été demandé de faire un site permettant la
-      création de listes de cadeaux pouvant être partagées.</p>
+     gestion des réservations de garage et de véhicule.</p>
 END;
     break;
 
@@ -56,8 +57,8 @@ END;
     $html .= <<<END
     <h1>Besoin d'aide ?</h1>
     <h3>Comment ca marche ?</h3>
-    <p>garagesolidaire est un outil vous permettant de créer une ou plusieurs listes de cadeaux que vous souhaitez partagées.<BR>
-      Vous pouvez également consulter les listes qui vous ont été partagé.<BR>
+    <p>Le Garage Solidaire est un outil vous permettant deréserver des ateliers d’entretien mécanique et des
+véhicules d’un garage associatif et participatif<BR>
       Avant toute chose, il faut vous créer un compte ou vous connectez si cela est déjà fait.<BR>
       Pour ce faire, il suffit de cliquer sur les onglets en haut à droite de la page.<BR></p>
 END;
