e5c8903b442a14af9f92043cdc9b5c2ba7c14e3f
Julien Claisse <julien.claisse2@etu.univ-lorraine.fr>
2018-02-08 10:02:49 +0100

    ADD BDD

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 require_once 'vendor/autoload.php' ;
 
 $db=new DB();
-$db->addConnection(parse_ini_file('./src/conf/conf.ini'));
+$db->addConnection(parse_ini_file('/conf/conf.ini'));
 $db->setAsGlobal();
 $db->bootEloquent();
 session_start();
