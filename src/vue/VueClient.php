01ff213ca261486def570dc2904694170e2e0fc9
Sarah Combe Deschaumes <sarah.combe-deschaumes6@etu.univ-lorraine.fr>
2018-02-08 11:45:19 +0100

    ajoute des boutons

--- src/vue/VueClient.php
+++ src/vue/VueClient.php
@@ -11,12 +11,35 @@ class VueClient{
   }
   
   public function afficherItem(){
+	  $id=$this->infos['id'];
 	  $nom=$this->infos['nom'];
 	  $desc=$this->infos['description'];
 	  $img=$this->infos['img'];
 	  
 	  $code="Nom : ".$nom." <br> Description : ".$desc;
 		$code=$code.'<img src="../../../img/'.$img.'" width = "150" height="150"></img><br>';
+		
+		//liens des boutons bidons
+		$buttonlisteres=<<<END
+<form id="listeres" method="get" action ="afficherlisteres/$id">
+<button type="submit" name="valider_affichage_liste_res" value="valid_affichage_liste_res">Afficher la liste des réservations</button>
+</form>
+END;
+$buttonplanning=<<<END
+<form id="planninggraph" method="get" action ="afficherplanninggraph/$id">
+<button type="submit" name="valider_affichage_planning_graph" value="valid_affichage_planning_graph">Planning graphique</button>
+</form>
+END;
+
+$buttonformulaireres=<<<END
+<form id="formulaireres" method="get" action ="afficherformulaireres/$id">
+<button type="submit" name="valider_affichage_formulaire_res" value="valid_affichage_formulaire_res">Réserver</button>
+</form>
+END;
+		$code=$code.$buttonlisteres.$buttonplanning.$buttonformulaireres;
+		
+		
+		
 		return $code;	  
   }
 
