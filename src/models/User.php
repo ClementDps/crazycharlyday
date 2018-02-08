55085ec9ddf50642c80175435c7cfd84e6b7ac2d
Clement Domps <domps1u@etu.univ-lorraine.fr>
2018-02-08 10:20:50 +0100

    ajoute de quoi se connecter

---
+++ src/models/User.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace garagesolidaire\models;
+
+class User extends \Illuminate\Database\Eloquent\Model{
+
+	protected $table='user';
+	protected $primaryKey='id';
+	public $timestamps=false;
+
+	public static function insert($nom,$prenom,$email,$mdp,$rang){
+		$hash=password_hash($mdp,PASSWORD_DEFAULT,['cost'=>12]);
+		$u=new User();
+		$u->nom=$nom;
+		$u->prenom=$prenom;
+		$u->email=$email;
+		$u->mdp=$hash;
+    $u->rang=0;
+		$u->save();
+	}
+
+	public static function mettreAjour($nom,$prenom,$amdp,$nmdp){
+		$u=User::find($_SESSION['userid']);
+		if(password_verify($amdp,$u->mdp)){
+			$u->nom=$nom;
+			$u->prenom=$prenom;
+			if($nmdp!=""){
+				$u->mdp=password_hash($nmdp,PASSWORD_DEFAULT,['cost'=>12]);
+			}
+			$u->save();
+		}
+	}
+}
