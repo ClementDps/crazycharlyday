d8eadfec00ee2a6c2eb3135f0ad058946b30d7c8
Clement Domps <domps1u@etu.univ-lorraine.fr>
2018-02-08 10:34:35 +0100

    ajoute les renders

--- src/vue/VueClient.php
+++ src/vue/VueClient.php
@@ -5,5 +5,32 @@ namespace garagesolidaire\vue;
 class VueClient{
 
 
+  public function render($int){
+  switch($int){
+    case 1:{
+      $content=$this->afficherAccueil();
+      break;
+    }
+  case 2:{
 
+      break;
+    }
+  }
+  $code= <<<END
+  <!DOCTYPE html>
+  <html>
+      <head>
+          <title>GarageSolidaire</title>
+          <meta charset="UTF-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <link rel="stylesheet" href="../style.css" />
+      </head>
+      <body>
+
+      </body>
+  </html>
+
+END;
+  echo $code;
+}
 }
