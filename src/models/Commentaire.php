e5910fd393c9370dc33699ec89858dcb48923fda
Sarah Combe Deschaumes <sarah.combe-deschaumes6@etu.univ-lorraine.fr>
2018-02-08 18:00:06 +0100

    commence les commentaires

---
+++ src/models/Commentaire.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace garagesolidaire\models;
+
+class Commentaire extends \Illuminate\Database\Eloquent\Model{
+
+  protected $table='Commentaire';
+	protected $primaryKey='id';
+	public $timestamps=false;
+
+  public function insert($user,$item,$message){
+    $c=new Commentaire();
+	$c->idUser=$user;
+	$c->idItem=$item;
+	$c->message=$message;
+	$c->dateMess=date("Y-m-d H:i:s",time());
+	$c->save();
+  }
+}
