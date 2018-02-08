c737861ec0a9e0e207aa17e4affc4a33fe3b0b6c
Julien Claisse <julien.claisse2@etu.univ-lorraine.fr>
2018-02-08 11:16:41 +0100

    ADD Page d'accueil

--- index.php
+++ index.php
@@ -1,15 +1,41 @@
 <?php
 require_once 'vendor/autoload.php' ;
 
+use \Slim\Slim;
+use Illuminate\Database\Capsule\Manager as DB;
+use garagesolidaire\controleur\GestionAccueil;
+
+
 $db=new DB();
-$db->addConnection(parse_ini_file('/conf/conf.ini'));
+$db->addConnection(parse_ini_file('conf/conf.ini'));
 $db->setAsGlobal();
 $db->bootEloquent();
+
 session_start();
-$app = new \Slim\Slim() ;
+$app = new Slim() ;
 
+//----------------------Pages-d-accueil--------------//
+$app->get('/', function () {
+  $c = new GestionAccueil();
+  $c -> afficheAccueil();
+})->name("accueil");
 
-// Redirection Erreur 404 
+$app->get('/contact', function () {
+  $c = new GestionAccueil();
+  $c -> afficheContact();
+})->name("contact");
+
+$app->get('/about', function () {
+  $c = new GestionAccueil();
+  $c -> afficheAbout();
+})->name("about");
+
+$app->get('/help', function () {
+  $c = new GestionAccueil();
+  $c -> afficheHelp();
+})->name("help");
+
+// Redirection Erreur 404
 // $app->notFound(function () use ($app) {
 //   $app->redirect($app->urlFor('list-not-found'));
 // });
