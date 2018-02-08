fc25552818d6e8e11101f261d77385f2cd0eed50
Clement Domps <domps1u@etu.univ-lorraine.fr>
2018-02-08 10:01:12 +0100

    ajoute la bdd

---
+++ src/model/Item.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace garagesolidaire\models;
+
+class Item extends \Illuminate\Database\Eloquent\Model{
+
+  protected $table='item';
+	protected $primaryKey='id';
+	public $timestamps=false;
+
+
+}
